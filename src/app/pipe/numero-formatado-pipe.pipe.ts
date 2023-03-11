435d59d6e8e0471aceb555fc29327c5d852eac76
Alan <alanbritodossantos2015@gmail.com>
2023-03-11 03:14:13 -0300

    ajuste

--- src/app/pipe/numero-formatado-pipe.pipe.ts
+++ src/app/pipe/numero-formatado-pipe.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'numeroFormatadoPipe'
+  name: 'numeroFormatadoPipe',
 })
 export class NumeroFormatadoPipePipe implements PipeTransform {
-
   transform(value: number): string {
-    const unidades = ['', 'mil', 'milhões', 'bilhões', 'trilhões', 'quatrilhões', 'quintilhões'];
+    const unidades = [
+      '',
+      'mil',
+      'milhões',
+      'bilhões',
+      'trilhões',
+      'quatrilhões',
+      'quintilhões',
+    ];
     let index = 0;
 
     while (value >= 1000 && index < unidades.length - 1) {
@@ -14,7 +21,10 @@ export class NumeroFormatadoPipePipe implements PipeTransform {
       index++;
     }
 
-    return value.toFixed(0) + ' ' + unidades[index];
+    if (value < 1000) {
+      return value.toFixed(0) + ' ' + unidades[index];
+    } else {
+      return (value / 1000).toFixed(1) + ' ' + unidades[index];
+    }
   }
-
 }
